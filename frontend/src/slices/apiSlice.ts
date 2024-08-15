97fd664db40a9c2a6d3a7b6d44632add333ac427
alexander-zhong <alexanderzousky@gmail.com>
2024-08-15 15:37:30 -0700

    cors fix attempt

--- frontend/src/slices/apiSlice.ts
+++ frontend/src/slices/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://flowspaceapi-alexander-zhongs-projects.vercel.app",
+  baseUrl: "https://flowspaceapi.vercel.app",
 });
 
 export const apiSlice = createApi({
